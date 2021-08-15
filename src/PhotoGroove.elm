10520247d698eceaed8119bc8572de772e0f16f8
jmtes <juliennetesoro@gmail.com>
2021-08-15 09:59:44 -0700

    Create initial model

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -31,5 +31,12 @@ view model =
         ]
 
 
+initialModel =
+    [ { url = "1.jpeg" }
+    , { url = "2.jpeg" }
+    , { url = "3.jpeg" }
+    ]
+
+
 main =
-    view "no model yet"
+    view initialModel

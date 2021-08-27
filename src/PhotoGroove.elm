23f1f3cfa1fc5e3abc9ca285fd4a65dbdf392be4
jmtes <juliennetesoro@gmail.com>
2021-08-26 22:24:14 -0700

    Add slide listener to filters

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -142,13 +142,14 @@ viewSizeChooser selectedSize size =
 -- not to use that name for custom properties
 
 
-viewFilter : String -> Int -> Html Msg
-viewFilter name magnitude =
+viewFilter : (Int -> Msg) -> String -> Int -> Html Msg
+viewFilter toMsg name magnitude =
     div [ class "filter-slider" ]
         [ label [] [ text name ]
         , rangeSlider
             [ Attr.max "11"
             , Attr.property "val" (Encode.int magnitude)
+            , onSlide toMsg
             ]
             []
         , label [] [ text (String.fromInt magnitude) ]
